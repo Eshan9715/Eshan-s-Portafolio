0e3cb9efe8afaa8f87386c4fb6866dbfb71c7247
Eshan Amanda <eshanesn97@gmail.com>
2022-07-08 20:31:08 +0530

    Adding resume details

---
+++ src/Components/Resume/Card.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+const Card = (props) => {
+  return (
+    <>
+      <div className='box btn_shadow'>
+        <div className='title_content d_flex'>
+          <div className='title'>
+            <h2>{props.title}</h2>
+            <span>{props.year}</span>
+            <img src={props.image} alt='' />
+          </div>
+        </div>
+        <hr />
+        <p>{props.desc}</p>
+      </div>
+    </>
+  )
+}
+
+export default Card
---
+++ src/Components/Resume/ResumeApi.js
@@ -0,0 +1,50 @@
+const ResumeApi = [
+  {
+    id: 1,
+    category: "education",
+    year: "Taxila Central College (2008 - 2017)",
+    title: "Secondary education",
+    desc: "The education should be very interactual. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
+    rate: "4.30/5",
+  },
+  {
+    id: 2,
+    category: "education",
+    year: "Wisdom Buisness Academy (2018 - 2019) ",
+    title: "Management Accounting",
+    desc: "Maecenas finibus nec sem ut imperdiet. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
+    rate: "4.50/5",
+  },
+  {
+    id: 3,
+    category: "education",
+    year: "University of Colombo (2019 - 2024) ",
+    title: "Electronics & IT ",
+    desc: "If you are going to use a passage. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
+    rate: "4.80/5 ",
+  },
+  {
+    id: 4,
+    category: "experience",
+    year: "Flutter ,  React-Native ",
+    title: "Mobile Alication Development",
+    desc: "Contrary to popular belief. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
+    
+  },
+  {
+    id: 5,
+    category: "experience",
+    year: "Adobe XD ,  PS ",
+    title: "UI / UX Design",
+    desc: "Generate Lorem Ipsum which looks. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
+  },
+  {
+    id: 6,
+    category: "experience",
+    year: "React JS ,  Next JS ",
+    title: "Web Development",
+    desc: "Maecenas finibus nec sem ut imperdiet. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
+  },
+]
+
+export default ResumeApi
