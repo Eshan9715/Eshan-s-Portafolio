64ae79b05c412968728be1972bc38680eb98a9ee
Eshan Amanda <eshanesn97@gmail.com>
2022-07-11 11:00:04 +0530

    Adding contact section

---
+++ src/Components/Testimonial/TestimonialApi.js
@@ -0,0 +1,63 @@
+const TestimonialApi = [
+  {
+    id: 1,
+    image: "https://icons-for-free.com/iconfiles/png/512/calculator+math+icon-1320086721646396340.png",
+    design: "Native Android",
+    name: "Mevine Thoda",
+    offcer: "Marketing Officer",
+    post: "Scientific Calculater",
+    date: "Mobile Application - Jun 3, 2022",
+    desc: "Marcent Of Vanice and treatment. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante. Ut tincidunt est ac dolor aliquam sodales phasellus smauris",
+    link: "https://github.com/Eshan9715/Scientific-Calculator",
+  },
+  {
+    id: 2,
+    image: "https://icons-for-free.com/iconfiles/png/512/coffee+cup+drink+espresso+mug+icon-1320165689647004961.png",
+    design: "C#",
+    name: "Davei Luace",
+    offcer: "Chief Operating Manager",
+    post: "Coffee cafe",
+    date: "Desktop Aplication - Jun 1, 2022",
+    desc: "When managment is so important. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante. Ut tincidunt est ac dolor aliquam sodales phasellus smauris.",
+    link: "https://github.com/Eshan9715/CoffeeApp",
+
+  },
+  {
+    id: 3,
+    image: "https://icons-for-free.com/iconfiles/png/512/hierarchical+hierarchy+order+organization+structure+team-1320184217204767744.png",
+    design: "Native Android",
+    name: "Nevine Acotanza",
+    offcer: "Chief Operating Officer",
+    post: "Data Algorithms Project",
+    date: "Mobile Application - Jun 3, 2022 - Aug 30, 2022",
+    desc: "Maecenas finibus nec sem ut imperdiet. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante. Ut tincidunt est ac dolor aliquam sodales phasellus smauris.",
+    link: "https://github.com/Eshan9715/Data-SA-Project",
+
+  },
+  {
+    id: 4,
+    image: "https://icons-for-free.com/iconfiles/png/512/weight-1324760637034794606.png",
+    design: "Flutter",
+    name: "Jone Duone Joe",
+    offcer: "Operating Officer",
+    post: "BMI Calculator",
+    date: "Mobile Application - Jun 3, 2022 - Aug 30, 2022",
+    desc: "Important fact to nec sem ut imperdiet. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante. Ut tincidunt est ac dolor aliquam sodales phasellus smauris.",
+    link: "https://github.com/Eshan9715/BMI-Calculator",
+
+  },
+  {
+    id: 5,
+    image: "https://icons-for-free.com/iconfiles/png/512/healthcare+medical+care+sun+thermometer+icon-1320195461662364608.png",
+    design: "Native Android",
+    name: "Nevine Dhawan",
+    offcer: "CEO Of Officer",
+    post: "Temp Mobile",
+    date: "Mobile Application - Jun 3, 2022 - Aug 30, 2022",
+    desc: "No more question for design. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante. Ut tincidunt est ac dolor aliquam sodales phasellus smauris	.",
+    link: "https://github.com/Eshan9715/TempMobile",
+
+  },
+]
+
+export default TestimonialApi
